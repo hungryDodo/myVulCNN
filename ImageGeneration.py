d0839ff9198c50b97514bd8159f161c62f448640
Dodo <gr_yao@126.com>
2024-12-12 20:10:45 +0800

    更新了多种中心性的转换

--- ImageGeneration.py
+++ ImageGeneration.py
@@ -1,20 +1,21 @@
-import networkx as nx
-import numpy as np
-import argparse
 import os
-import sent2vec
-import pickle
 import glob
-from multiprocessing import Pool, Value, Lock
+import pickle
+import argparse
+import sent2vec
+import numpy as np
 from tqdm import tqdm
+import networkx as nx
+from prettytable import PrettyTable
+from multiprocessing import Pool, Value, Lock
 
 
 # 保证 centrality 输入参数有效
 def validate_centrality(value):
-    valid_characters = set('BCDEP')
+    valid_characters = set('BCDEKP')
     if not set(value).issubset(valid_characters) or len(value) != len(set(value)):
         raise argparse.ArgumentTypeError(
-            f"Invalid centrality type '{value}'. Must be a subset of non-repeating letters from 'BCDEP'."
+            f"Invalid centrality type '{value}'. Must be a subset of non-repeating letters from 'BCDEKP'."
         )
     return value
 
@@ -26,183 +27,94 @@ def parse_options():
     parser.add_argument('-m', '--model', help='The path of model.', required=True)
     # 增加一个控制 centrality 种类的选项, 默认使用5种 centrality
     parser.add_argument(
-        '-c', '--centrality', help='The type of centrality to use.', default='DPCEB', type=validate_centrality
+        '-c', '--centrality', help='The type of centrality to use.', default='BCDEP', type=validate_centrality
     )
     args = parser.parse_args()
     return args
 
 
-def graph_extraction(dot):
-    try:
-       graph = nx.drawing.nx_pydot.read_dot(dot)
-    except Exception as e:
-        # 捕获并处理异常，打印详细的错误信息
-        print(f'An exception occurred: {e}')
-    # 使用 NetworkX 从.dot文件中读取图。
-    # graph = nx.drawing.nx_pydot.read_dot(dot)
+def save_info_file(output_dir, args):
+    # Get the current time and format it
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    #TAG 新增
-    # graph = nx.drawing.nx_agraph.read_dot(dot)
-    #BUG return none
-    return graph  # 返回读取的图对象。
+    # Info to save in Markdown table format
+    info = f"# Run Information\n\n"
+    info += "| **Parameter**       | **Value**                         |\n"
+    info += "|---------------------|-----------------------------------|\n"
+    info += f"| Generation Time     | {current_time}                   |\n"
+    info += f"| Input Directory     | {args.input}                     |\n"
+    info += f"| Output Directory    | {args.out}                       |\n"
+    info += f"| Model Path          | {args.model}                     |\n"
+    info += f"| Centrality Types    | {args.centrality}                |\n"
+    
+    # Define the info file path
+    info_file = os.path.join(output_dir, "info.md")
+    
+    # Check if the file exists, append if it does, otherwise create it
+    if os.path.exists(info_file):
+        with open(info_file, 'a') as f:
+            f.write("\n" + "---" + "\n\n")  # Use Markdown separator
+            f.write(info)
+    else:
+        with open(info_file, 'w') as f:
+            f.write(info)
 
 
 def sentence_embedding(sentence):
-    # 使用 sent2vec_model 对句子进行嵌入处理，得到其向量表示。
     emb = sent2vec_model.embed_sentence(sentence)
-    return emb[0]  # 返回嵌入的第一个向量（假设结果是一个二维数组）。
-
-# def image_generation(dot):
-#     try:
-#         # 尝试从.dot文件中提取图。
-#         #BUG 提取图发生了报错
-#         pdg = graph_extraction(dot)
-#         # 获取图中节点的属性（这里获取的是节点的标签）。
-#         labels_dict = nx.get_node_attributes(pdg, 'label')
-#         labels_code = dict()  # 初始化一个字典以存储节点标签和代码映射。
-#         for label, all_code in labels_dict.items():
-#             # 从标签字符串中提取代码片段并进行一些替换操作。
-#             #BUG 源代码这一句可能会造成准确率ACC的错误
-#             #DONE 解决，ACC错误原因是由于.dot文件生成为空，这里直接使用的作者已经生成的自己的.dot文件，没有自己生成
-#             code = all_code[all_code.index(",") + 1:-2].split('\\n')[0]
-            
-#             #MARK 来的issue中的修改----没有生效
-#             # code = all_code[all_code.index(",") + 1:-1].split('\n')[0]
-            
-#             code = code.replace("static void", "void")
-#             labels_code[label] = code  # 将处理后的代码映射到对应的节点标签。
-
-#         # 打印节点标签与代码映射（调试用）。
-#         #print(labels_code)
-#         # 计算图的度中心性。
-#         degree_cen_dict = nx.degree_centrality(pdg)
-#         # 计算图的接近中心性。
-#         closeness_cen_dict = nx.closeness_centrality(pdg)
-#         # 计算图的谐波中心性（注释掉）。
-#         #harmonic_cen_dict = nx.harmonic_centrality(pdg)
-
-#         # 创建一个有向图 G 并添加节点和边。
-#         G = nx.DiGraph()
-#         G.add_nodes_from(pdg.nodes())
-#         G.add_edges_from(pdg.edges())
-#         # 计算图的Katz中心性。
-#         katz_cen_dict = nx.katz_centrality(G)
-        
-#         # 打印中心性字典（调试用）。
-#         # print(degree_cen_dict)
-#         # print(closeness_cen_dict)
-#         # print(harmonic_cen_dict)
-#         # print(katz_cen_dict)
-
-#         degree_channel = []  # 初始化度中心性通道列表。
-#         closeness_channel = []  # 初始化接近中心性通道列表。
-#         katz_channel = []  # 初始化Katz中心性通道列表。
-        
-
-#         for label, code in labels_code.items():
-#             # 对节点代码进行句子嵌入处理，得到其向量。
-#             line_vec = sentence_embedding(code)
-#             line_vec = np.array(line_vec)  # 将嵌入向量转换为 NumPy 数组。
-
-#             # 获取该节点的度中心性并与嵌入向量相乘，结果添加到相应通道。
-#             degree_cen = degree_cen_dict[label]
-#             degree_channel.append(degree_cen * line_vec)
-
-#             # 获取该节点的接近中心性并与嵌入向量相乘，结果添加到相应通道。
-#             closeness_cen = closeness_cen_dict[label]
-#             closeness_channel.append(closeness_cen * line_vec)
-
-#             # 获取该节点的Katz中心性并与嵌入向量相乘，结果添加到相应通道。
-#             katz_cen = katz_cen_dict[label]
-#             katz_channel.append(katz_cen * line_vec)
-
-#         return (degree_channel, closeness_channel, katz_channel)  # 返回生成的三个通道。
-#     except:
-#         return None  # 处理失败时返回 None。
+    return emb[0]
 
 
-def image_generation(dot):
+def image_generation(dot, centrality_types):
     try:
-        # 尝试从.dot文件中提取图。
-        pdg = graph_extraction(dot)
-        # 获取图中节点的属性（这里获取的是节点的标签）。
+        pdg = nx.drawing.nx_pydot.read_dot(dot)
         labels_dict = nx.get_node_attributes(pdg, 'label')
-        labels_code = dict()  # 初始化一个字典以存储节点标签和代码映射。
+        labels_code = dict()
         for label, all_code in labels_dict.items():
-            # 从标签字符串中提取代码片段并进行一些替换操作。
-            #DONE 解决，ACC错误原因是由于.dot文件生成为空，这里直接使用的作者已经生成的自己的.dot文件，没有自己生成
             code = all_code[all_code.index(",") + 1:-2].split('\\n')[0]
-            
-            #MARK 来的issue中的修改----没有生效
-            # code = all_code[all_code.index(",") + 1:-1].split('\n')[0]
-            
             code = code.replace("static void", "void")
-            labels_code[label] = code  # 将处理后的代码映射到对应的节点标签。
+            labels_code[label] = code
 
-        # 打印节点标签与代码映射（调试用）。
-        #print(labels_code)
-        # 计算图的度中心性。
-        degree_cen_dict = nx.degree_centrality(pdg)
-        # 计算图的接近中心性。
-        closeness_cen_dict = nx.closeness_centrality(pdg)
-        # 计算图的谐波中心性（注释掉）。
-        #harmonic_cen_dict = nx.harmonic_centrality(pdg)
-        
+        # 初始化存储通道的字典
+        channels_dict = {
+            'B': [],  # Betweenness centrality
+            'C': [],  # Closeness centrality
+            'D': [],  # Degree centrality
+            'E': [],  # Eigenvector centrality
+            'K': [],  # Katz centrality
+            'P': []   # PageRank centrality
+        }
 
-        # 创建一个有向图 G 并添加节点和边。
-        G = nx.DiGraph()
-        G.add_nodes_from(pdg.nodes())
-        G.add_edges_from(pdg.edges())
-        # # 计算图的Katz中心性。
-        # katz_cen_dict = nx.katz_centrality(G)
-        
-        #MARK 计算pageRank中心性
-        try:  
-            pageRank_cen_dict = nx.pagerank(G)  
-        except Exception as e:  
-            print(f"计算 PageRank 时发生错误：{e}")
-        
-        # print(f'\033[31m' + 'pageRank_cen_dict:' + pageRank_cen_dict + '\033[0m')
-        
-        # 打印中心性字典（调试用）。
-        # print(degree_cen_dict)
-        # print(closeness_cen_dict)
-        # print(harmonic_cen_dict)
-        # print(katz_cen_dict)
+        # 计算指定中心性
+        centrality_dicts = {}
+        if 'B' in centrality_types:
+            centrality_dicts['B'] = nx.betweenness_centrality(pdg)
+        if 'C' in centrality_types:
+            centrality_dicts['C'] = nx.closeness_centrality(pdg)
+        if 'D' in centrality_types:
+            centrality_dicts['D'] = nx.degree_centrality(pdg)
+        if 'E' in centrality_types:
+            centrality_dicts['E'] = nx.eigenvector_centrality(pdg)
+        if 'K' in centrality_types:
+            centrality_dicts['K'] = nx.katz_centrality(pdg)
+        if 'P' in centrality_types:
+            centrality_dicts['P'] = nx.pagerank(pdg)
 
-        degree_channel = []  # 初始化度中心性通道列表。
-        closeness_channel = []  # 初始化接近中心性通道列表。
-        # katz_channel = []  # 初始化Katz中心性通道列表。
-        pageRank_channel = [] #MARK 初始化pageRank中心性通道列表
-
-       
-
+        # 计算通道向量
         for label, code in labels_code.items():
-            # 对节点代码进行句子嵌入处理，得到其向量。
             line_vec = sentence_embedding(code)
-            line_vec = np.array(line_vec)  # 将嵌入向量转换为 NumPy 数组。
+            line_vec = np.array(line_vec)
 
-            # 获取该节点的度中心性并与嵌入向量相乘，结果添加到相应通道。
-            degree_cen = degree_cen_dict[label]
-            degree_channel.append(degree_cen * line_vec)
+            for centrality_type in centrality_types:
+                if centrality_type in centrality_dicts:
+                    cen_value = centrality_dicts[centrality_type].get(label, 0)
+                    channels_dict[centrality_type].append(cen_value * line_vec)
 
-            # 获取该节点的接近中心性并与嵌入向量相乘，结果添加到相应通道。
-            closeness_cen = closeness_cen_dict[label]
-            closeness_channel.append(closeness_cen * line_vec)
+        return [channels_dict[ctype] for ctype in centrality_types]
 
-            # # 获取该节点的Katz中心性并与嵌入向量相乘，结果添加到相应通道。
-            # katz_cen = katz_cen_dict[label]
-            # katz_channel.append(katz_cen * line_vec)
-
-            #MARK 获取该节点的PageRank中心性并与嵌入向量相乘，结果添加到相应通道。
-            pageRank_cen = pageRank_cen_dict[label]
-            pageRank_channel.append(pageRank_cen * line_vec)
-
-        print('\033[31m' + 'pageRank_cen' + '\033[0m')
-        return (degree_channel, closeness_channel, pageRank_channel)  # 返回生成的三个通道。
-    except:
-        print('\033[32m' + '绿色文本' + '\033[0m')
-        return None  # 处理失败时返回 None。
+    except Exception as e:
+        print(f"\033[31mError: {e}\033[0m")
+        return
 
 
 def write_to_pkl(dot_file, output_dir, existing_files, centrality, progress_counter, lock):
@@ -219,11 +131,9 @@ def write_to_pkl(dot_file, output_dir, existing_files, centrality, progress_coun
     if channels is None:
         return None
 
-    degree_channel, closeness_channel, pageRank_channel = channels
     out_pkl = os.path.join(output_dir, f"{file_basename}.pkl")
-    data = [degree_channel, closeness_channel, pageRank_channel]
     with open(out_pkl, 'wb') as f:
-        pickle.dump(data, f)
+        pickle.dump(channels, f)
 
 
 def validate_and_normalize_paths(input_path, output_path, model_path):
@@ -252,7 +162,25 @@ def extract_pkl_filenames(output_dir):
 def main():
     args = parse_options()
     input_dir, output_dir, trained_model_path = validate_and_normalize_paths(args.input, args.out, args.model)
+
+    centrality_names = {
+        'B': 'Betweenness Centrality',
+        'C': 'Closeness Centrality',
+        'D': 'Degree Centrality',
+        'E': 'Eigenvector Centrality',
+        'K': 'Katz Centrality',
+        'P': 'PageRank Centrality'
+    }
+    table = PrettyTable()
+    table.field_names = ["Short Name", "Full Name"]
+    for ctype in centrality_types:
+        table.add_row([ctype, centrality_names[ctype]])
+    print("本次使用的中心性列表：")
+    print(table)
     
+    # 保存一个 info 文件
+    save_info_file(output_dir, args)
+
     global sent2vec_model
     sent2vec_model = sent2vec.Sent2vecModel()
     sent2vec_model.load_model(trained_model_path)
